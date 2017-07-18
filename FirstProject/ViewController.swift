53d99085cf05bee6fe6817ba89427e8110328770
Franklyn Taylor <Franklyn@franklyns-air>
2017-07-17 20:02:15 -0400

    Initial Commit

---
+++ FirstProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstProject
+//
+//  Created by Franklyn Taylor on 7/17/17.
+//  Copyright © 2017 functional. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
